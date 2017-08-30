bf4e90bc96524b38613b86393767a85fd62a284c
Victor Manuel García Sanabria <vik000@gmail.com>
2017-08-30 13:59:54 +0200

    gulp import incorporado. Lo siguiente es intentar instalar bootstrap y que funcione

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var gulpImport = require("gulp-html-import");
 
 
 gulp.task("sass",function(){
   gulp.src("./src/scss/style.scss")
   .pipe(sass())
   .pipe(gulp.dest("./dist/css/"))
+});
+
+//copiar e importar html:
+gulp.task("html",function(){
+  gulp.src("src/*.html")
+    .pipe(gulpImport("src/components/"))//ojo con la barra final, que debe ser imprescindible por ser una carpeta.
+    .pipe(gulp.dest("dist/"))
 });
